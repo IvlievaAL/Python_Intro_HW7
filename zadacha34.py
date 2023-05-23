d1e8be0ab087a1e5ac84479e6706a3be9e796eeb
AlexIv <ivlieva.medphyslab.com>
2023-05-23 19:50:42 +0300

    z34 code works: version without list comprehension

--- zadacha34.py
+++ zadacha34.py
@@ -12,3 +12,33 @@
 
 # **Ввод:** пара-ра-рам рам-пам-папам па-ра-па-да    
 #     **Вывод:** Парам пам-пам 
+
+# А теперь коротко Суть(ТМ) задачи:
+# С клавиатуры вводят поочередно строки, состоящие из подстрок, разделенных пробелом.
+# Надо всего лишь подсчитать количество гласных в каждой подстроке.
+
+# def is_our_word_cyrillic(our_word, points_cyril, points_latin):
+#     kirillitsa = ("АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ")
+#     is_our_word_cyrillic_char = [char for char in kirillitsa if char in our_word]
+#     if is_our_word_cyrillic_char:
+#         points = points_cyril
+#     else:
+#         points = points_latin
+#     return points
+
+russian_vowels_characters = ("АЕИОУЭЮЯ")
+our_phrases = input("Вводи кричалку, Винни: ").upper().split()
+how_many_vowels = []
+for phrase in our_phrases:
+    vowels_in_phrase = 0
+    for char in phrase:
+        if char in russian_vowels_characters:
+            vowels_in_phrase += 1
+    how_many_vowels.append(vowels_in_phrase)
+print(how_many_vowels)
+if 0 in how_many_vowels:
+    raise ValueError("Нет гласных в одной из фраз!")
+if len(set(how_many_vowels)) == 1: 
+    print("Парам пам-пам")
+else:
+    print("Пам парам" )
